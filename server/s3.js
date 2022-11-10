bc2d3001efd94791d6dc505eabd089d9e3c5c58b
Wesley Preece <w.preece@cliqdigital.com>
2022-11-10 17:40:33 +0100

    ready1

--- server/s3.js
+++ server/s3.js
@@ -5,10 +5,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const bucketName = process.env.BUCKET_NAME;
+const region = process.env.BUCKET_REGION;
+const accessKeyId = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 
 const s3Client = new S3Client({
   region,
