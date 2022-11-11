c8c1d05bba0116b3f969d7d38dfce88a1e77e170
Wesley Preece <w.preece@cliqdigital.com>
2022-11-11 17:06:04 +0100

    ready9

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,17 @@ import CardSection from "../components/CardSection/CardSection";
 import Hero from "../components/HeroSection/Hero";
 import { getPlatforms } from "../server/platforms.js";
 import { useRouter } from "next/router";
+import CookieConsent from "react-cookie-consent";
+
+export function enableGoogleAdsense() {
+  const head = document.getElementsByTagName("head")[0];
+  const scriptElement = document.createElement(`script`);
+  scriptElement.type = `text/javascript`;
+  scriptElement.async;
+  scriptElement.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_ID}`;
+  scriptElement.crossOrigin = "anonymous";
+  head.appendChild(scriptElement);
+}
 
 const Home = (props) => {
   const [platforms, setPlatforms] = useState(props.platforms);
@@ -14,12 +25,28 @@ const Home = (props) => {
         <title>Streaming Compare</title>
         <meta name="description" content="Streaming Compare" />
         <link rel="icon" href="/favicon.ico" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9013062630732036"
-          crossorigin="anonymous"
-        ></script>
       </Head>
+      <CookieConsent
+        location="bottom"
+        buttonText="Accept"
+        cookieName="CookieConsent"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+        onAccept={() => enableGoogleAdsense()}
+      >
+        This website uses cookies to enhance the user experience.{" "}
+        <span
+          style={{ fontSize: "10px", cursor: "pointer" }}
+          onClick={() => {
+            router.push({
+              pathname: "/privacy",
+            });
+          }}
+        >
+          More Information
+        </span>
+      </CookieConsent>
       <section className={""}>
         <Hero />
       </section>
