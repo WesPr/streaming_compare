a8898cac521dff5f84c6c1b2a75fb9f20fbed4a2
Wesley Preece <w.preece@cliqdigital.com>
2022-11-12 14:52:23 +0100

    ready12

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,6 @@ import CardSection from "../components/CardSection/CardSection";
 import Hero from "../components/HeroSection/Hero";
 import { getPlatforms } from "../server/platforms.js";
 import { useRouter } from "next/router";
-import CookieConsent from "react-cookie-consent";
-
-export function enableGoogleAdsense() {
-  const head = document.getElementsByTagName("head")[0];
-  const scriptElement = document.createElement(`script`);
-  scriptElement.type = `text/javascript`;
-  scriptElement.async;
-  scriptElement.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_ID}`;
-  scriptElement.crossOrigin = "anonymous";
-  head.appendChild(scriptElement);
-}
 
 const Home = (props) => {
   const [platforms, setPlatforms] = useState(props.platforms);
@@ -26,35 +15,16 @@ const Home = (props) => {
         <meta name="description" content="Streaming Compare" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <CookieConsent
-        location="bottom"
-        buttonText="Accept"
-        cookieName="CookieConsent"
-        style={{ background: "#2B373B" }}
-        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-        expires={150}
-        onAccept={() => enableGoogleAdsense()}
-      >
-        This website uses cookies to enhance the user experience.{" "}
-        <span
-          style={{ fontSize: "10px", cursor: "pointer" }}
-          onClick={() => {
-            router.push({
-              pathname: "/privacy",
-            });
-          }}
-        >
-          More Information
-        </span>
-      </CookieConsent>
-      <section className={""}>
+      <section>
         <Hero />
       </section>
       <section className={" bg-[rgb(0,0,0)]"}>
         <CardSection platforms={platforms} />
       </section>
       <p
-        className={"bg-[rgb(0,0,0)] text-white text-sm p-5 cursor-pointer"}
+        className={
+          "bg-[rgb(0,0,0)] text-white text-sm p-5 cursor-pointer mt-auto"
+        }
         onClick={() => {
           router.push({
             pathname: "/privacy",
